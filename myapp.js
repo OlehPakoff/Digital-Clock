ec965782944136a07a8d54aeddc857b4a6869e4f
Oleg Panchenko <thepanchenko@gmail.com>
2022-01-13 22:55:24 +0200

    js moved to new file

--- myapp.js
+++ myapp.js
@@ -1,6 +1,6 @@
-function updateclock(){
+function updateClock(){
     var now = new Date();
-    var dname = now.detDay(),
+    var dname = now.getDay(),
         mo = now.getMonth(),
         dnum = now.getDate(),
         yr = now.getFullYear(),
@@ -9,15 +9,28 @@ function updateclock(){
         sec = now.getSeconds(),
         pe = "AM";
 
+        if(hou == 0){
+            hou = 12;
+        }
+        if(hou > 12){
+            hou = hou - 12;
+            pe = "PM";
+        }
+
+        Number.prototype.pad = function(digits){
+            for (var n = this.toString(); n.length < digits; n = 0 + n);
+            return n;
+        }
+
         var months = ["Junuary", "February", "March", "April", "May", "June", "July", "Augest", "September", "October", "November", "December"];
         var week = ["Monday", "Thusday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
         var ids = ["dayname", "month", "daynum", "year", "hour", "minutes", "seconds", "period"];
-        var values = [dname, mo, dnum, yr, hou, min, sec, pe];
-        for(var i=0; i < 0 ids.length; i++)
+        var values = [week[dname], months[mo], dnum.pad(2), yr.pad(2), hou.pad(2), min.pad(2), sec.pad(2), pe];
+        for (var i = 0; i < ids.length; i++)
         document.getElementById(ids[i]).firstChild.nodeValue = values[i];
 }
 
 function initClock(){
-    updateclock();
+    updateClock();
     window.setInterval("updateClock()", 1);
 }
